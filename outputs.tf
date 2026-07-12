cda110e708515ac42927a0ec8d1e44f0ca921caa
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,26 @@
-output "api_management_redis_caches" {
-  description = "All api_management_redis_cache resources"
-  value       = azurerm_api_management_redis_cache.api_management_redis_caches
-  sensitive   = true
-}
 output "api_management_redis_caches_api_management_id" {
-  description = "List of api_management_id values across all api_management_redis_caches"
-  value       = [for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_redis_caches, keyed the same as var.api_management_redis_caches"
+  value       = { for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : k => v.api_management_id }
 }
 output "api_management_redis_caches_cache_location" {
-  description = "List of cache_location values across all api_management_redis_caches"
-  value       = [for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : v.cache_location]
+  description = "Map of cache_location values across all api_management_redis_caches, keyed the same as var.api_management_redis_caches"
+  value       = { for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : k => v.cache_location }
 }
 output "api_management_redis_caches_connection_string" {
-  description = "List of connection_string values across all api_management_redis_caches"
-  value       = [for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : v.connection_string]
+  description = "Map of connection_string values across all api_management_redis_caches, keyed the same as var.api_management_redis_caches"
+  value       = { for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : k => v.connection_string }
   sensitive   = true
 }
 output "api_management_redis_caches_description" {
-  description = "List of description values across all api_management_redis_caches"
-  value       = [for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : v.description]
+  description = "Map of description values across all api_management_redis_caches, keyed the same as var.api_management_redis_caches"
+  value       = { for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : k => v.description }
 }
 output "api_management_redis_caches_name" {
-  description = "List of name values across all api_management_redis_caches"
-  value       = [for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : v.name]
+  description = "Map of name values across all api_management_redis_caches, keyed the same as var.api_management_redis_caches"
+  value       = { for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : k => v.name }
 }
 output "api_management_redis_caches_redis_cache_id" {
-  description = "List of redis_cache_id values across all api_management_redis_caches"
-  value       = [for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : v.redis_cache_id]
+  description = "Map of redis_cache_id values across all api_management_redis_caches, keyed the same as var.api_management_redis_caches"
+  value       = { for k, v in azurerm_api_management_redis_cache.api_management_redis_caches : k => v.redis_cache_id }
 }
 
